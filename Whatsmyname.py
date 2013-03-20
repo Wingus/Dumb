1ed47cb0dc847c1633ffda593cb2c3ff1b9a04c0
Lou Remy <Louisremy96@gmail.com>
2013-03-19 20:39:58 -0400

    What's my name?

---
+++ Whatsmyname.py
@@ -0,0 +1,10 @@
+def clinic():
+	print "What is my name?"
+	answer = raw_input("Type my name, and hit enter!")
+	if answer == "Louis" or answer == "louis"
+		print "Correct!"
+	else:
+		print "WRONG! Try again... you wingus."
+		clinic()
+		
+clinic()
